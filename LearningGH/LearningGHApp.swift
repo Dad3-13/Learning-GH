cefe6e489362c14b71d74b74dbb850ca782bb3f2
Dade <dade_beast@hotmail.it>
2022-09-26 13:56:35 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Davide Bettarello on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
