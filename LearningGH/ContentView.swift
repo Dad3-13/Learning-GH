49125ad5425660c7ac9b6268be179e19fc689839
Dad3-13 <114405012+Dad3-13@users.noreply.github.com>
2022-09-27 13:59:55 +0200

    Update ContentView.swift

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Davide!")
+            Text("Hello, Figaro!")
         }
         .padding()
     }
